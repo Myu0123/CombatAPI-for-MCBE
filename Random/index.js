5dc814b5ec5101a518b05c50487f70a71dc8827e
Mannyu Thanai <chiayu.myu@icloud.com>
2024-09-17 18:00:23 +0800

    Used for random generating numbers

--- Random/index.js
+++ Random/index.js
@@ -1,13 +1,26 @@
-class LCGSeed{
-  constructor(seed){
+/*try{
+  return operation();
+}
+catch(error){
+  return NaN;
+}*/
+class LCG_SeededRNG{
+  constructor(seed){ //seed不可為0
     this.max = 2147483647;
     this.period = 16807;
-    this.seed = Math.floor(seed) % this.max;
-    if(this.seed <= 0) this.seed += this.max-1;
+
+    if(typeof seed === 'number' && isFinite(seed)){
+      this.seed = Math.floor(seed) % this.max;
+      while(this.seed <= 0) this.seed += this.max-1; //[1, max)
+    }
+    else{
+      this.seed = Math.floor(Math.random() * this.max)
+    }
+    this.RN = (this.seed-1) / (this.max-1);
   }
   next(){
     this.seed = (this.seed * this.period) % this.max;
-    return (this.seed - 1) / this.max-1;
+    this.RN = (this.seed-1) / (this.max-1);
   }
 }
 
@@ -17,20 +30,31 @@ class Weight{
     this.w = weights;
     this.t = times;
     this.total = weights.reduce((sum, weight) => sum + weight, 0);
-    this.pick();
   }
-  pick(){
+  generate(){
     let result = [];
 
     for(let i=0; i<this.t; i++){
-      let RDnum = Math.random() * this.total;
+      let RN = Math.random() * this.total;
       let cmW = 0;
 
       for(let j=0; j<this.c.length; j++){
         cmW += this.w[j];
-        if(RDnum < cmW) result.push(this.c[j]);
+        if(RN < cmW) result.push(this.c[j]);
       }
     }
     return result;
+  }
+}
+
+class Rarity{
+  constructor(maximum){
+    this.m = maximum;
+  }
+  generate(){
+    let RN = Math.random() * this.m;
+
+    if(RN === 0) return Infinity;
+    else return 1 / RN;
   }
 }
